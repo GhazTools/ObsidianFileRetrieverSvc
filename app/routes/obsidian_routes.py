136f87aa750a7b983ecc805ccabda4407ed8d4c0
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-06-24 22:35:42 -0400

    bug fix

--- app/routes/obsidian_routes.py
+++ app/routes/obsidian_routes.py
@@ -32,7 +32,7 @@ def get_file_list(request: Request) -> HTTPResponse:
 @OBSIDIAN_ROUTES_BLUEPRINT.post("/getFileContents")
 def get_file_contents(request: Request) -> HTTPResponse:
     vault: Vault = request.app.config["VAULT"]
-    file_name = request.args.get("fileName")  # Returns the first value for 'filename'
+    file_name = request.json.get("fileName")  # Returns the first value for 'filename'
 
     return json({"file_contents": vault.get_vault_file_contents_by_name(file_name)})
 
@@ -40,14 +40,14 @@ def get_file_contents(request: Request) -> HTTPResponse:
 @OBSIDIAN_ROUTES_BLUEPRINT.post("/getFileContentsDetailed")
 def get_file_contents_detailed(request: Request) -> HTTPResponse:
     vault: Vault = request.app.config["VAULT"]
-    file_name = request.args.get("fileName")  # Returns the first value for 'filename'
+    file_name = request.json.get("fileName")  # Returns the first value for 'filename'
 
-    return json({"file_contents": vault.get_vault_file_contents_by_name(file_name)})
+    return json({"file_contents": vault.get_file_contents_by_name_detailed(file_name)})
 
 
 @OBSIDIAN_ROUTES_BLUEPRINT.get("/getFolderContents")
 def get_folder_contents(request: Request) -> HTTPResponse:
     vault: Vault = request.app.config["VAULT"]
-    folder_name = request.args.get("folderName")
+    folder_name = request.json.get("folderName")
 
     return json(vault.get_folder_contents(folder_name))
