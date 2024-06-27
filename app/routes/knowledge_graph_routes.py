bf45c69d3e3e7e7abca252a3a3a7f04092b57dad
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2024-06-26 21:25:04 -0400

    knoweldge graph logic

---
+++ app/routes/knowledge_graph_routes.py
@@ -0,0 +1,20 @@
+"""
+file_name = knowledge_graph_routes.py
+Created On: 2024/06/26
+Lasted Updated: 2024/06/26
+Description: _FILL OUT HERE_
+Edit Log:
+2024/06/26
+    - Created file
+"""
+
+# STANDARD LIBRARY IMPORTS
+from sanic.import import Blueprint
+
+# THIRD PARTY LIBRARY IMPORTS
+
+# LOCAL LIBRARY IMPORTS
+
+KNOWLEDGE_GRAPH_BLUEPRINT = Blueprint("knowledge_graph_blueprint", url_prefix="/")
+
+
